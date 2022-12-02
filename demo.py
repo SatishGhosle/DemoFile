7afe18b636aac68a93434454b6c7f2a255bc8f7d
Satish <satishghosle01@gmail.com>
2022-12-02 23:42:04 +0530

    Second commit

--- demo.py
+++ demo.py
@@ -9,3 +9,13 @@ def create_socket(add_fam = -1,tcp_protocol = -1):
     except socket.error as err:
         print(f"Socket could not be created due to {err}")
     return s
+
+def get_hostIP(host_name):
+    try:
+        hostIP = socket.gethostbyname(host_name)
+    except socket.gaierror:
+        print("Error occured while resolving the host")
+        sys.exit()
+    
+    if hostIP:
+        return hostIP
